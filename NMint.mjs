401b2683a07dd5fcc1f078dc8403058881aeea96
nblurr <admin@nblurr.com>
2024-08-06 22:10:03 -0400

    Correction on mint logics

--- NMint.mjs
+++ NMint.mjs
@@ -207,7 +207,7 @@ export class NMint {
 	           }
 	           
 	           console.log("Targeted N mint price : " + actualPricePerNTarget + ", Actual estimated cost per N : " + pricePerN);
-	           if(pricePerN > actualPricePerNTarget && pricePerN <= this.targetLimitPrice){
+	           if(pricePerN <= actualPricePerNTarget && pricePerN <= this.targetLimitPrice){
 	                this.mintCount = (this.mintCount ==2 ? 0: this.mintCount+1);
 
 	                console.log('Mint price target reached! A tx will be mined : Mintable N: ' + nbMintable + ' Estimated $/N: ' + pricePerN + ' Total est. mint cost $: ' + this.mintCost);
